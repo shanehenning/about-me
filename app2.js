f47da49b742e39fb67c91dff75080f4ca547c0d2
Shane Henning <shanerhenning@gmail.com>
2016-04-13 23:07:12 -0700

    finalized js for wednesdays project

--- app2.js
+++ app2.js
@@ -1,42 +1,56 @@
-var i = 0;
 function gameClick(){
-
   var userName = prompt('Hi! What is your name?');
   console.log('The user said their name was ' + userName);
   var game = alert('Hi ' + userName + ', you seem like a fun human! Wanna play a super fun game?');
-  console.log('The user says ' + game);
 
-  var questionBank = ['Question 1 of 5: \rOk, ' + userName + ', Yes or No: I was raised by wolves?', 'Question 2 of 5: \rI\'m allergic to jalapenos?', 'Question 3 of 5: \rI am a descendent of Abraham Lincoln?', 'Question 4 of 5: \rI have a pet chameleon?', 'Last Question!\rThis one\'s worth 3x\'s as many points as the others!\r\rI\'m a phenomenal dancer?', 'Guess my favorite number!\r\r(Hint: It\'s between 1-10)'];
-  var commentaryYes = ['Turns out, no, I was raised by real people.\r\rOk, here comes the next question!', 'Ha! I am not! Some like it hot, and I am that some!\r\rAlright here\'s the next one!', 'I cannot lie, I am not related to the Vampire Slayer.\r\rHow about another question?!', 'I wish! My landlord doesn\'t allow awesome!\r\rReady for the last question? I bet you are!', 'My whiteness prohibits any dancing skill.'];
-  var commentaryNo = ['That\'s right! I was raised by real people!\r\rOk, here comes the next question!', 'Correct ' + userName + '! And thank goodness! Nachos wouldn\'t be the same!\r\rAlright here\'s the next one!', 'Lucky guess? You got it ' + userName + '!\r\rHow about another question?!', 'You\'re so good at this game ' + userName + '!\rAlas my home is not that exciting...\r\rReady for the last question? I bet you are!', 'Too true. Too true.', 'That\'s not a number! Let\'s try that again!'];
-  for (i = 0; i < questionBank.length; i++) {
-    var response = prompt(questionBank[i]);
+  var questionBank = ['Question 1 of 7: \rOk, ' + userName + ', Yes or No: I was raised by wolves?', 'Question 2 of 7: \rI\'m allergic to jalapenos?', 'Question 3 of 7: \rI am a descendent of Abraham Lincoln?', 'Question 4 of 7: \rI have a pet chameleon?', 'Question 5 of 7:\rI\'m a phenomenal dancer?'];
+  var commentaryYes = ['Turns out, no, I was raised by real people.\r\rOk, here comes the next question!', 'Ha! I am not! Some like it hot, and I am that some!\r\rAlright here\'s the next one!', 'I cannot lie, I am not related to the Vampire Slayer.\r\rHow about another question?!', 'I wish! My landlord doesn\'t allow awesome!\r\rHere comes the next question!'];
+  var commentaryNo = ['That\'s right! I was raised by real people!\r\rOk, here comes the next question!', 'Correct ' + userName + '! And thank goodness! Nachos wouldn\'t be the same!\r\rAlright here\'s the next one!', 'Lucky guess? You got it ' + userName + '!\r\rHow about another question?!', 'You\'re so good at this game ' + userName + '!\rAlas my home is not that exciting...\r\rHere comes the next question!', 'Too true. Too true.', 'That\'s not a number! Let\'s try that again!'];
+  var favColor = ['blue', 'red', 'purple'];
+  var q = 0;
+
+//Questions 1-5
+  for (x = 0; x < questionBank.length; x++) {
+    var response = prompt(questionBank[x]);
     console.log('The user says ' + response);
     if (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes'){
-      alert(commentaryYes[i]);
+      alert(commentaryYes[x]);
     } else{
-      alert(commentaryNo[i]);
+      alert(commentaryNo[x]);
+      q += 1;
     }
   }
-
-  while (i < 3){
-    var response = prompt('hi');
-    i ++;
-    if(response === 4){
-      alert('You did it!');
-      break;
-    } else if (response.isNaN()){
-      alert(commentaryNo[6]);
-    } else if(response < 4){
+//Question 6
+  var y = 0;
+  while (y < 3){
+    y ++;
+    var response = prompt('Question 6 of 7:\rCan you guess my favorite number!\r\r(I\'ll give you four tries! Hint: It\'s between 1-10)');
+    console.log('The user says ' + response);
+    if(parseInt(response) < 7){
       alert('Too low!');
-    } else if(response > 4){
+    } else if(response > 7){
       alert('Too high!');
+    } else if (isNaN(response)){
+      alert('That\'s not a number!');
+    } else{
+      y = 3;
+      q += 1;
+      alert('That\'s right! ' + response + ' is my favorite number!\r\rAre you ready for the last question? I bet you are!');
     }
-    else{
+  }
+//Question 7
+  var z = 0;
+  while (z < 5){
+    z ++;
+    var response = prompt('Question 7 of 7:\rCan you guess one of my favorite colors?\r\rThis time I\'ll give you six tries.');
+    console.log('The user says ' + response);
+    if (response == favColor[0] || response == favColor[1] || response == favColor[2]){
+      z = 5;
+      q += 1;
+      alert('That\'s right!, ' + response + ' is one of my favorite colors!\r\rThat was the last one! Let\'s see how you did!');
+    } else {
       alert('Try again!');
     }
   }
-  // alert('You did it!\rThat took about as long as the DMV.\rWay to power through!');
-
+  alert('Way to go!  You got ' + q + ' out of 7 questions correct!\r\rThat\'s ' + parseInt(q / 7 * 100) + '%!');
 }
-()
